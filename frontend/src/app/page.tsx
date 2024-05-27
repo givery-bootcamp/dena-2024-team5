1e6d220363ea824c8918ecfa71e39c6f39131d52
fastsnowy <61731151+fastsnowy@users.noreply.github.com>
2024-05-27 17:20:24 +0900

    postedItemに対するリファクタリング

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,37 +1,40 @@
-import { get } from "http";
-import Image from "next/image";
 import PostedItem from "@/components/PostedItem";
-import { PostedItemType } from "@/types/PostedItemType";
+import type { PostedItemType } from "@/types/PostedItemType";
+import Image from "next/image";
 
 async function getData() {
-  const res = await fetch('http://localhost:9000/hello')
- 
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data')
-  }
- 
-  return res.json()
+	const res = await fetch("http://localhost:9000/hello");
+
+	if (!res.ok) {
+		// This will activate the closest `error.js` Error Boundary
+		throw new Error("Failed to fetch data");
+	}
+
+	return res.json();
 }
 
-
 export default async function Home() {
-  const data = await getData();
-  const mockData: PostedItemType = {
-    ID: 0,
-    Title: "test-title",
-    Body: "text-text",
-    UserId: 0,
-    UserName: "hogehoge",
-    CreatedAt: new Date(),
-    UpdatedAt: new Date()
-  }
+	const data = await getData();
+	const mockData: PostedItemType = {
+		ID: 0,
+		Title: "test-title",
+		Body: "text-text",
+		UserId: 0,
+		UserName: "hogehoge",
+		CreatedAt: new Date(),
+		UpdatedAt: new Date(),
+	};
 
-  const listData = [mockData, mockData, mockData];
-  console.log(data);
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {listData.map(data => <PostedItem  postedItem={data}></PostedItem> )}
-    </main>
-  );
+	const listData = [mockData, mockData, mockData];
+	console.log(data);
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			{listData.map((data, idx) => (
+				// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+				<div key={idx}>
+					<PostedItem postedItem={data} />
+				</div>
+			))}
+		</main>
+	);
 }
--- frontend/src/components/PostedItem.tsx
+++ frontend/src/components/PostedItem.tsx
@@ -1,13 +1,13 @@
+import type { PostedItemType } from "@/types/PostedItemType";
 import Link from "next/link";
-import { PostedItemType } from "@/types/PostedItemType";
 
-export default async function PostedItem({postedItem}:{postedItem:PostedItemType}) {
-
-    return (
-        <div>
-        <Link href={"#"}>{postedItem.Title}</Link>
-        {postedItem.UpdatedAt.toLocaleDateString()}
-        </div>
-    );
-  }
-  
+export default async function PostedItem({
+	postedItem,
+}: { postedItem: PostedItemType }) {
+	return (
+		<div>
+			<Link href={"#"}>{postedItem.Title}</Link>
+			{postedItem.UpdatedAt.toLocaleDateString()}
+		</div>
+	);
+}
