fb01d45ca3f08ad7cc666b00b478345646a9a29b
mouchi <mouchimouchi00@gmail.com>
2024-05-27 17:07:13 +0900

    create postedItemType

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,7 @@
 import { get } from "http";
 import Image from "next/image";
 import PostedItem from "@/components/PostedItem";
+import { PostedItemType } from "@/types/PostedItemType";
 
 async function getData() {
   const res = await fetch('http://localhost:9000/hello')
@@ -16,11 +17,21 @@ async function getData() {
 
 export default async function Home() {
   const data = await getData();
-  const listData = [data, data, data, data];
+  const mockData: PostedItemType = {
+    ID: 0,
+    Title: "test-title",
+    Body: "text-text",
+    UserId: 0,
+    UserName: "hogehoge",
+    CreatedAt: new Date(),
+    UpdatedAt: new Date()
+  }
+
+  const listData = [mockData, mockData, mockData];
   console.log(data);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {listData.map(data => <PostedItem  message={data.message}></PostedItem> )}
+      {listData.map(data => <PostedItem  postedItem={data}></PostedItem> )}
     </main>
   );
 }
