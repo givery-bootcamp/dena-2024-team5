b03579bc9e191a2f03b026ee557394584f9dbb09
fastsnowy <61731151+fastsnowy@users.noreply.github.com>
2024-06-11 11:50:12 +0900

    Update linter rules to include "noUnusedImports" for better code correctness

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
-import LeftMenu from "@/components/LeftMenu";
 import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
