54e57baa5be52eebf3c5e73b5957afd2073972df
mouchi <mouchimouchi00@gmail.com>
2024-06-17 14:25:44 +0900

    refactor post detail page

--- frontend/src/app/(user)/posts/[id]/page.tsx
+++ frontend/src/app/(user)/posts/[id]/page.tsx
@@ -1,4 +1,8 @@
-export default function Home({ params }: { params: { id: string } }) {
+import { aspidaClient } from "@/lib/aspidaClient";
+
+export default async function Home({ params }: { params: { id: string } }) {
+  const postedData = await aspidaClient.post._postId(Number(params.id)).$get();
+
   return (
     <div>
       投稿詳細画面<div>{params.id}</div>
