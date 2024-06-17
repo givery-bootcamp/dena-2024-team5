4f05b5a41e1a2dbe1131433110c50dfebebe86b3
txxxxc <tomoya113.code@gmail.com>
2024-06-17 17:25:23 +0900

    add: UserGetMeUsecase

---
+++ backend/internal/usecases/user_get_me.go
@@ -0,0 +1,20 @@
+package usecases
+
+import (
+	"myapp/internal/entities"
+	"myapp/internal/interfaces"
+)
+
+type UserGetMeUsecase struct {
+	repository interfaces.UserGetDetailRepository
+}
+
+func NewUserGetMeUsecase(r interfaces.UserGetDetailRepository) *UserGetMeUsecase {
+	return &UserGetMeUsecase{
+		repository: r,
+	}
+}
+
+func (p *UserGetMeUsecase) Execute(id int) (*entities.User, error) {
+	return p.repository.GetDetail(id)
+}
