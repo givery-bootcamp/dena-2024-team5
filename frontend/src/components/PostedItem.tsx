7a60d0ccfa76fb2277f7396f3cff249298f4e347
mouchi <mouchimouchi00@gmail.com>
2024-06-17 14:13:35 +0900

    pnpm format

--- frontend/src/components/PostedItem.tsx
+++ frontend/src/components/PostedItem.tsx
@@ -7,9 +7,9 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import type {Entities_Post} from "../../api/@types/"
-import Link from "next/link";
 import { dateFormatString2DateJa } from "@/utils/date";
+import Link from "next/link";
+import type { Entities_Post } from "../../api/@types/";
 
 export default async function PostedItem({
   postedItem,
@@ -26,7 +26,9 @@ export default async function PostedItem({
       </CardContent>
       <CardFooter className="flex justify-between">
         <p className="text-sm">{postedItem.username}</p>
-        <p className="text-sm">{dateFormatString2DateJa(postedItem.created_at)}</p>
+        <p className="text-sm">
+          {dateFormatString2DateJa(postedItem.created_at)}
+        </p>
         <Button asChild>
           <Link href={`posts/${postedItem.id}`}>詳細</Link>
         </Button>
