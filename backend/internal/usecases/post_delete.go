f023d8e4054bcfffcae16fd00a14682e5e122fcf
tsuyoshi.nakai <tsuyoshi.nakai@dena.com>
2024-06-18 15:12:10 +0900

    add post delete usecase

---
+++ backend/internal/usecases/post_delete.go
@@ -0,0 +1,19 @@
+package usecases
+
+import (
+	"myapp/internal/interfaces"
+)
+
+type PostDeleteUsecase struct {
+	repository interfaces.PostDeleteRepository
+}
+
+func NewPostDeleteUsecase(r interfaces.PostDeleteRepository) *PostDeleteUsecase {
+	return &PostDeleteUsecase{
+		repository: r,
+	}
+}
+
+func (p *PostDeleteUsecase) Execute(postID uint) error {
+	return p.repository.PostDelete(postID)
+}
