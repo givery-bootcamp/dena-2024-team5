f9fa05c508efb6414566a595d0fa7795f73b9ff6
hasegawa282 <likes7nana@gmail.com>
2024-06-24 14:19:49 +0900

    frontの差分なくす

--- frontend/src/app/(user)/page.tsx
+++ frontend/src/app/(user)/page.tsx
@@ -1,10 +1,8 @@
 import PostedItem from "@/components/PostedItem";
-// import { aspidaClient } from "@/lib/aspidaClient";
-import { Entities_Post } from "../../../api/@types";
+import { aspidaClient } from "@/lib/aspidaClient";
 
 export default async function Home() {
-  // const listData = await aspidaClient.posts.$get();
-  const listData:Entities_Post[] = []
+  const listData = await aspidaClient.posts.$get();
 
   return (
     <div className="grid grid-cols-1 gap-4">
