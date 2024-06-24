2b3ba153042cccd5860948a134678f91e511e18c
tsuyoshi.nakai <tsuyoshi.nakai@dena.com>
2024-06-24 15:53:58 +0900

    add comment model and entity

---
+++ backend/internal/repository/model/comment.go
@@ -0,0 +1,27 @@
+package model
+
+import (
+	"myapp/internal/entity"
+
+	"gorm.io/gorm"
+)
+
+type Comment struct {
+	gorm.Model
+	Body   string
+	UserID uint
+	User   User
+	PostID uint
+	Post   Post
+}
+
+func ConvertCommentModelToEntity(c *Comment) *entity.Comment {
+	return &entity.Comment{
+		ID:        c.ID,
+		Body:      c.Body,
+		UserID:    c.UserID,
+		PostID:    c.PostID,
+		CreatedAt: c.CreatedAt,
+		UpdatedAt: c.UpdatedAt,
+	}
+}
