ad59d614321b126951c9b56255ef1a77b24b90fc
mouchi <mouchimouchi00@gmail.com>
2024-06-25 16:54:01 +0900

    フロント　コメント削除機能

--- frontend/src/components/CommentItem.tsx
+++ frontend/src/components/CommentItem.tsx
@@ -1,5 +1,3 @@
-"use client";
-import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -7,22 +5,17 @@ import {
   CardFooter,
 } from "@/components/ui/card";
 import { dateFormatString2DateJa } from "@/utils/date";
+import { cookies } from "next/headers";
 import type { Entity_Comment } from "../../api/@types";
-
-export const comment_Example: Entity_Comment = {
-  body: "コメント",
-  created_at: "2024-06-24T07:42:32.595Z",
-  id: 1,
-  post_id: 1,
-  updated_at: "2024-06-24T07:42:32.595Z",
-  user_id: 1,
-};
+import { CommentDeleteDialog } from "./deleteCommentDialog";
 
 export async function CommentItem({
   comment,
 }: {
   comment: Entity_Comment;
 }) {
+  const cookieStore = cookies();
+  const jwtToken = cookieStore.get("jwt")?.value ?? "";
   return (
     <Card className="w-full">
       <CardContent>
@@ -31,10 +24,7 @@ export async function CommentItem({
       <CardFooter className="flex justify-between">
         <p className="text-sm">{comment.user_id}</p>
         <p className="text-sm">{dateFormatString2DateJa(comment.created_at)}</p>
-        <Button onClick={() => console.log("削除ボタン")}>
-          削除
-          {/* <Link href={`posts/${comment.id}`}>削除</Link> */}
-        </Button>
+        <CommentDeleteDialog commentId={comment.id} jwtToken={jwtToken} />
       </CardFooter>
     </Card>
   );
