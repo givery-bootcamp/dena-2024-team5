8e665c9df40eae05e2dfc378d7a3065be4ee6f9f
tsuyoshi.nakai <tsuyoshi.nakai@dena.com>
2024-06-25 10:53:24 +0900

    add comment owner validate usecase

--- backend/internal/repository/comment.go
+++ backend/internal/repository/comment.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	"errors"
+	"myapp/internal/entity"
 	"myapp/internal/repository/model"
 
 	"gorm.io/gorm"
@@ -16,7 +18,21 @@ func NewCommentRepository(conn *gorm.DB) *CommentRepository {
 	}
 }
 
-func (c *CommentRepository) CommentNew(userID, postID uint, body string) error {
+// idに該当するcommentがないとき、返り値は (nil, nil) になる。
+// errorはこれ以外の内部エラーが起きたときだけ入る。
+func (r *CommentRepository) GetDetail(commentID uint) (*entity.Comment, error) {
+	var obj model.Comment
+	result := r.Conn.Preload("User").Preload("Post").Where("id = ?", commentID).First(&obj)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, result.Error
+	}
+	return model.ConvertCommentModelToEntity(&obj), nil
+}
+
+func (r *CommentRepository) CommentNew(userID, postID uint, body string) error {
 	comment := model.Comment{
 		Body:   body,
 		UserID: userID,
