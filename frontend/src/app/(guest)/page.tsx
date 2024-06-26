84bb27e2a2fabddecedd07ba0d6a8240a52b9a65
usuyuki <63891531+usuyuki@users.noreply.github.com>
2024-06-26 16:32:46 +0900

    feat 任意の画像を動いているふうにするやつ

--- frontend/src/app/(guest)/page.tsx
+++ frontend/src/app/(guest)/page.tsx
@@ -1,5 +1,4 @@
 import { auth } from "@/auth";
-import Header from "@/components/Header";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
@@ -9,7 +8,6 @@ export default async function Home() {
   }
   return (
     <div>
-      <Header />
       <div className="grid grid-cols-1 gap-4 p-4">
         <p>未ログイン</p>
       </div>
