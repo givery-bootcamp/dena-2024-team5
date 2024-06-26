2fbe543b6359133ecc926ddc84df1177d3b38cc5
usuyuki <63891531+usuyuki@users.noreply.github.com>
2024-06-26 14:07:26 +0900

    fix いらない最低限

--- frontend/src/app/(guest)/page.tsx
+++ frontend/src/app/(guest)/page.tsx
@@ -12,15 +12,6 @@ export default async function Home() {
       <Header />
       <div className="grid grid-cols-1 gap-4 p-4">
         <p>未ログイン</p>
-        <div className="card nes-container is-rounded" style="width: 35rem;">
-          <div className="card-body">
-            <h5 className="card-title">NES.css</h5>
-            <p className="card-text">
-              NES.css is a NES-style (8bit-like) CSS Framework. NES.css only
-              requires CSS and doesn't depend on any JavaScript.
-            </p>
-          </div>
-        </div>
       </div>
     </div>
   );
