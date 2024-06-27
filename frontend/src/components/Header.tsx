ffbe3ac74818211489689ca862fe49e655655af3
fastsnowy <61731151+fastsnowy@users.noreply.github.com>
2024-06-27 18:57:36 +0900

    ⚡️ コメントアウト削除

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,13 +5,12 @@ import { SignInSignOutButton } from "./Buttons";
 export default async function Header() {
   const session = await auth();
   return (
-    <header className="flex items-center justify-between p-4 border-b">
+    <header className="flex items-center justify-between p-4 border-b bg-white">
       <Link href="/">
         <h1 className="text-2xl font-bold">Header</h1>
       </Link>
       <div className="flex items-center space-x-4">
         <SignInSignOutButton session={session} />
-        {/* <ModeToggle /> */}
       </div>
     </header>
   );
