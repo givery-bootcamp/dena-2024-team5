25c6837cf78f447eaa37c5c5f80cca8b8723b893
usuyuki <63891531+usuyuki@users.noreply.github.com>
2024-06-28 15:41:27 +0900

    feat ダッシュボードレイヤーわけ

---
+++ frontend/src/components/dots/organism/dashboard/DashBoardBackgroundLayer.tsx
@@ -0,0 +1,10 @@
+"use client";
+import MapTileGrid from "../../molecule/MapTileGrid";
+
+export default function DashboardBackgroundLayer() {
+  return (
+    <div className="grid-master map-tile">
+      <MapTileGrid />
+    </div>
+  );
+}
