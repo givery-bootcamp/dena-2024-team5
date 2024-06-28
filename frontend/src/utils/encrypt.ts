5598249169ebc2eec0a89e88121e9c5d85bc26d5
fastsnowy <61731151+fastsnowy@users.noreply.github.com>
2024-06-28 11:23:37 +0900

    🚑 lintエラーをignore

--- frontend/src/utils/encrypt.ts
+++ frontend/src/utils/encrypt.ts
@@ -1,4 +1,5 @@
-import { createHash } from "node:crypto";
+// biome-ignore lint/style/useNodejsImportProtocol: <explanation>
+import { createHash } from "crypto";
 export const saltAndHash = (str: string): string => {
   const salt = process.env.AUTH_SECRET;
   const hash = createHash("sha256");
